a007167ba2d9d90585e3be4646a80ed948f1a782
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-02-12 12:39:34 +0200

    changed email validation rules to not fail tests from time to time

--- schemas/post-user.js
+++ schemas/post-user.js
@@ -1,6 +1,6 @@
 var Joi = require('joi');
 
 module.exports = Joi.object().keys({
-    email: Joi.string().email().min(3).max(30).required(),
+    email: Joi.string().email().min(3).max(60).required(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
 }).with('email', 'password');
